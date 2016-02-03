71825454484b501bc7c501e9d6ef5afcc3b8630b
Gambit <chernozor@yandex.ru>
2016-02-03 03:04:23 +0300

    release

---
+++ uptolike-share/uptolike_share_widget.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: UpToLike Social Share Buttons
+Plugin URI: http://uptolike.ru/
+Description: Uptolike Social Share Buttons - social bookmarking widget with sharing statistics.
+Version: 1.4.4
+Author: Uptolike
+Author URI: http://uptolike.ru/
+*/
+
+
+// Creating the widget 
+
+include 'widget_options.php';

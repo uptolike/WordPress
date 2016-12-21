cc508f18eb32da558378f2227fe930c0b125644a
gambit <chernozor@yandex.ru>
2016-12-21 15:39:07 +0300

    ver. 1.5.5

--- uptolike-share/uninstall.php
+++ uptolike-share/uninstall.php
@@ -3,7 +3,10 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) exit();
 
 function uptolike_delete_plugin() {
-    delete_option('my_option_name');
+    $delete = get_option('my_option_name');
+    $delete['uptolike_email'] = '';
+    $delete['id_number'] = '';
+    update_option('my_option_name', $delete);
 }
 
 uptolike_delete_plugin();
